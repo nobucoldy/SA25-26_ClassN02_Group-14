06d3e7819bea846efebcab6e0dc023238996f3c3
bac <quebac2385@gmail.com>
2026-01-07 07:48:53 +0700

    api chỉnh sửa thông tin

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\MovieController;
-use App\Http\Controllers\Api\ShowtimeController;
 use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\ShowtimeController;
 
 // Auth
 Route::post('/login', [AuthController::class, 'login']);
@@ -24,4 +25,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/bookings', [BookingController::class, 'create']);
     Route::delete('/bookings/{id}', [BookingController::class, 'cancel']);
     Route::get('/my-bookings', [BookingController::class, 'myBookings']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [ProfileController::class, 'me']);
+    Route::put('/me', [ProfileController::class, 'update']);
+    Route::put('/me/password', [ProfileController::class, 'changePassword']);
 });
